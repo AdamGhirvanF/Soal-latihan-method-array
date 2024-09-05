ee5159626adad1a1ee22d538be8140d938642480
AdamGhirvanF <adam_ghirvan@yahoo.co.id>
2024-09-05 11:04:16 +0700

    Submit

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const collectionNumber = [1, 2, 3, 4, 5, 6]
  * Buat array baru berisi angka2 yg merupakan hasil perkalian 3 dari setiap element array collectionNumber
  */
 
+const newCollectionNumber = collectionNumber.map(function(data){
+    return data * 3;
+});
 
+console.log("Hasil perkalian angka tiga: " + newCollectionNumber)
 
 const colors = ["blue", "black", "purple", "white", "pink"]
 /**
  * buat array baru yang isinya element yang hurufnya lebih dari 4 berdasarkan array colors
  */
+
+const newColors = colors.filter(function(data){
+    return data.length > 4;
+});
+
+console.log("Huruf lebih dari 4: " + newColors);
